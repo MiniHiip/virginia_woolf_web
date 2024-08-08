b4b776c75bd6c6eed197fbce81803c24961039d8
MiniHiip <160938919+MiniHiip@users.noreply.github.com>
2024-08-08 14:52:30 -0600

    footer structure

--- virginia_woolf_web/src/app/app.routes.ts
+++ virginia_woolf_web/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import path from 'path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'footer', component: FooterComponent}
+];
