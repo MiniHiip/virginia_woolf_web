4251b3f1ee210c873561c40f34d08bdea1dd988a
RealMeisner <129326965+RealMeisner@users.noreply.github.com>
2024-08-08 14:52:09 -0600

    Navbar v.1

---
+++ virginia_woolf_web/src/app/navbar-layout/navbar-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar-layout.component.html',
+  styleUrl: './navbar-layout.component.scss'
+})
+export class NavbarLayoutComponent {
+
+}
