43ef74911d958decf0315dca31d8b458630db2fc
MiniHiip <160938919+MiniHiip@users.noreply.github.com>
2024-08-20 12:48:52 -0600

    carrusel without functionality

--- virginia_woolf_web/src/app/about-s/about-s.component.ts
+++ virginia_woolf_web/src/app/about-s/about-s.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about-s',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './about-s.component.html',
   styleUrl: './about-s.component.scss'
 })
 export class AboutSComponent {
-
+  
 }
