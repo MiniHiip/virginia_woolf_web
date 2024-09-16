a9815e047be306d65b180ae198a65e52cb7b3a50
RealMeisner <129326965+RealMeisner@users.noreply.github.com>
2024-09-16 12:20:57 -0600

    First check on navbar function

--- virginia_woolf_web/src/main.ts
+++ virginia_woolf_web/src/main.ts
@@ -1,7 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter, Routes } from '@angular/router';
 import { AppComponent } from './app/app.component';
+import { FirstModuleComponent } from './app/first-module/first-module.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: 'inicio', component: FirstModuleComponent },
+  // { path: 'convocatorias', component: ConvocatoriasComponent },
+  // { path: 'conocenos', component: ConocenosComponent },
+  // { path: 'contactanos', component: ContactanosComponent }
+];
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes)
+  ]
+}).catch(err => console.error(err));
